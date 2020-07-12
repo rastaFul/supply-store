5b8974bdf303dbf0d61d690a31e82ab2da75806d
RoSampa94 <rodrigob.dev@outlook.com>
2020-07-12 19:11:51 -0300

    Adicionar o typeORM

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
+require('reflect-metadata');
+const { createConnection } = require('typeorm');
 const Server = require('./server/server');
 
 (async () => {
+  await createConnection();
   const app = new Server();
   app.listen();
 })();
